b4932f48698abc1492a364f83b8a37bb5577d1f7
Mateus Sandes <mateussandes@hotmail.com.br>
2024-01-06 20:00:59 -0300

    Classe de start foi resetada

---
+++ src/main/java/br/com/sandes/StartApp.java
@@ -0,0 +1,14 @@
+package br.com.sandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class StartApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartApp.class, args);
+	}
+
+}
