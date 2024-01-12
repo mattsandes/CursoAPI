531fcb358d70caf5ad54a425d4cf0d04ec93d288
Mateus Sandes <mateussandes@hotmail.com.br>
2024-01-12 02:28:00 -0300

    Renomeei o controller para PersonController e apaguei as classes de exceção

--- src/main/java/br/com/sandes/controllers/PersonController.java
+++ src/main/java/br/com/sandes/controllers/PersonController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class MathController {
+public class PersonController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
@@ -21,29 +21,5 @@ public class MathController {
             }
 
             return convertToDouble(numberOne) + convertToDouble(numberTwo);
-    }
-
-    private Double convertToDouble(String strNumber){
-        if(strNumber == null){
-            return 0D;
-        }
-
-        String number = strNumber.replaceAll(",", "."); //removendo a vigula e colocanddo o ponto;
-
-        if(isNumeric(number)){
-            return Double.parseDouble(number);
-        }
-
-        return 0D;
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if(strNumber == null){
-            return false;
-        }
-
-        String number = strNumber.replaceAll(",", ".");
-
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
