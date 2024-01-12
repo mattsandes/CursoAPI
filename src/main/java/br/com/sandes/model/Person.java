7f49f855687692d413054e1c85b6dcf0329da185
Mateus Sandes <mateussandes@hotmail.com.br>
2024-01-12 02:28:37 -0300

    Creiei a classe person que vai servir depois como entidade

---
+++ src/main/java/br/com/sandes/model/Person.java
@@ -0,0 +1,82 @@
+package br.com.sandes.model;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    private Long id;
+
+    private String first_name;
+
+    private String last_name;
+    private String address;
+    private String gender;
+    public Person() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (!id.equals(person.id)) return false;
+        if (!first_name.equals(person.first_name)) return false;
+        if (!last_name.equals(person.last_name)) return false;
+        if (!address.equals(person.address)) return false;
+        return gender.equals(person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + first_name.hashCode();
+        result = 31 * result + last_name.hashCode();
+        result = 31 * result + address.hashCode();
+        result = 31 * result + gender.hashCode();
+        return result;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
