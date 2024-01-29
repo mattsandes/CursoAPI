7450b698d9797396fb26bb933bf2bdf6b6eadcf6
Mateus Sandes <mateussandes@hotmail.com.br>
2024-01-29 20:28:40 -0300

    Adicionado a migration que vai criar a tabela no banco de dados

--- src/main/resources/db/migration/V1__Create_Table_Person.sql
+++ src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -2,5 +2,6 @@ CREATE TABLE person (
     id bigint NOT NULL,
     first_name character varying(80) NOT NULL,
     last_name character varying(80) NOT NULL,
-    address character varying(80) NOT NULL,
-    gender character varying(6) NOT NULL);
+    gender character varying(6) NOT NULL,
+    address character varying(80) NOT NULL
+);
