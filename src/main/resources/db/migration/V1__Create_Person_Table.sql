166f055ab3606be55e231c619b94a88131ac5ecf
Mateus Sandes <mateussandes@hotmail.com.br>
2024-02-06 02:28:35 -0300

    Migrations ajustadas

---
+++ src/main/resources/db/migration/V1__Create_Person_Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE person (
+    id SERIAL NOT NULL,
+    first_name character varying(80) NOT NULL,
+    last_name character varying(80) NOT NULL,
+    gender character varying(6) NOT NULL,
+    address character varying(80) NOT NULL
+);
---
+++ src/main/resources/db/migration/V2__Populate_Person_Table.sql
@@ -0,0 +1,7 @@
+INSERT INTO person (id, first_name, last_name, gender, address) VALUES
+(1, 'Simone', 'Simons', 'Female', 'Epica Street - 109, Holand'),
+(2, 'Tommy', 'Karevick', 'Male', 'House fo the Hill - Kamelot'),
+(3, 'Dave', 'Mustane', 'Male', 'Darkest Hour Street - EUA'),
+(4, 'Roy', 'Khan', 'Male', 'House fo the Hill - Kamelot'),
+(5, 'Floor', 'Jansen', 'Female', 'Her Walden'),
+(6, 'Tarja', 'Turunen', 'Female', 'Dark Chest of Wonders');
