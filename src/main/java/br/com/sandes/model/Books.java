b0a7ad86cdb73e78e33ee294961d520fe184a5b1
Mateus Sandes <mateussandes@hotmail.com.br>
2024-02-21 00:43:48 -0300

    Criado o a entidade modelo para Books

---
+++ src/main/java/br/com/sandes/model/Books.java
@@ -0,0 +1,116 @@
+package br.com.sandes.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "books")
+public class Books implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+
+    @Column(
+            name = "author",
+            nullable = false,
+            length = 80)
+    private String author;
+
+    @Column(
+            name = "price",
+            nullable = false)
+    private Double price;
+
+    @Column(
+            name = "launch_date",
+            nullable = false
+    )
+    private Date launchDate;
+
+    @Column(name = "title",
+            nullable = false,
+            length = 80)
+    private String title;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Books books = (Books) o;
+
+        if (id != books.id) return false;
+        if (!Objects.equals(author, books.author)) return false;
+        if (!Objects.equals(price, books.price)) return false;
+        if (!Objects.equals(launchDate, books.launchDate)) return false;
+        return Objects.equals(title, books.title);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (launchDate != null ? launchDate.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
+
+    public Books(long id, String author, Double price, String title) {
+        this.id = id;
+        this.author = author;
+        this.price = price;
+        this.title = title;
+    }
+
+    public Date getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Date launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public Books() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
