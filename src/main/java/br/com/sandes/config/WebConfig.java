d869bd153ee140b0c84b3fab0aea354a293df068
Mateus Sandes <mateussandes@hotmail.com.br>
2024-02-27 06:42:55 -0300

    Adicionando os cos para requisições

--- src/main/java/br/com/sandes/config/WebConfig.java
+++ src/main/java/br/com/sandes/config/WebConfig.java
@@ -1,16 +1,21 @@
 package br.com.sandes.config;
 
 import br.com.sandes.serializationconverter.YmlJackson2HttpConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration //serve para que quando o spring carregar, o spring leia esse arquivo pos contem informações de configuração da aplicação
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
 
     @Override
     public void configureContentNegotiation
@@ -33,5 +38,16 @@ public class WebConfig implements WebMvcConfigurer {
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
 
         converters.add(new YmlJackson2HttpConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+
+        registry.addMapping("/**")
+                //.allowedMethods("GET", "POST", "DELETE", "PUT") servem apenas como referencia;
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
