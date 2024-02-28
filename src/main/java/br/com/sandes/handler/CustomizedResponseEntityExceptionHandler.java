fae5f627616bd72a9bb16dc38c9b4095fc6f6e4c
Mateus Sandes <mateussandes@hotmail.com.br>
2024-02-28 12:49:15 -0300

    Criada o mapping para lidar com a classe InvalidJWTException

--- src/main/java/br/com/sandes/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/br/com/sandes/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,8 @@
 package br.com.sandes.handler;
 
-import java.util.Date;
-
+import br.com.sandes.exceptions.ExceptionResponse;
+import br.com.sandes.exceptions.InvalidJWTAuthenticationException;
+import br.com.sandes.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,8 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import br.com.sandes.exceptions.ExceptionResponse;
-import br.com.sandes.exceptions.ResourceNotFoundException;
+import java.util.Date;
 
 @ControllerAdvice
 @RestController
@@ -39,5 +39,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				request.getDescription(false));
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(InvalidJWTAuthenticationException.class)
+	private final ResponseEntity<ExceptionResponse> handleInvalidJWTAuthenticationExceptionException(
+			Exception ex, WebRequest request){
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 }
