30a20ed68757cf69b929af7f15000b599ef5e5a4
Mateus Sandes <mateussandes@hotmail.com.br>
2024-03-06 16:02:24 -0300

    Criado o repositorio para User

--- src/main/java/br/com/sandes/repositories/UserRepository.java
+++ src/main/java/br/com/sandes/repositories/UserRepository.java
@@ -1,12 +1,13 @@
 package br.com.sandes.repositories;
 
-import br.com.sandes.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import br.com.sandes.model.User;
 
-    @Query("SELECT u FROM Users WHERE u.userName =:userName")
-    User findByUserName(@Param("userName") String userName);
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	@Query("SELECT u FROM User u WHERE u.userName = :userName") //Query que vai pegar os usuarios pelo nome deles no banco de dados;
+	User findByUsername(@Param(value = "userName") String userName);
 }
