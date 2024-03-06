4839accf2c69ed51d6037ff908b722920a728ad8
Mateus Sandes <mateussandes@hotmail.com.br>
2024-03-06 15:53:33 -0300

    Adicionando as especificações para printar a senha no log da aplicação

--- src/main/java/br/com/sandes/StartApp.java
+++ src/main/java/br/com/sandes/StartApp.java
@@ -8,6 +8,26 @@ public class StartApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartApp.class, args);
+		
+//		Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = 
+//				new Pbkdf2PasswordEncoder("",
+//						8,
+//						185000,
+//						SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+//		
+//        Map<String, PasswordEncoder> encoders = new HashMap<>();
+//        
+//        encoders.put("pbkdf2", pbkdf2PasswordEncoder);
+//        
+//        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+//        
+//        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2PasswordEncoder);
+//        
+//        String result1 = passwordEncoder.encode("admin123");
+//        String result2 = passwordEncoder.encode("admin234");
+//        
+//        System.out.println("My hash " + result1);
+//        System.out.println("My hash " + result2);
 	}
 
 }
