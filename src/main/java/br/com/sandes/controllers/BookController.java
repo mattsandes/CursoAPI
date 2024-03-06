44b5fee2a181999877df05c703d64d54aaefc3eb
Mateus Sandes <mateussandes@hotmail.com.br>
2024-03-06 15:59:04 -0300

    Removidas as importações para anotations que eu pretendia usar para corrigir os warnings

--- src/main/java/br/com/sandes/controllers/BookController.java
+++ src/main/java/br/com/sandes/controllers/BookController.java
@@ -1,7 +1,19 @@
 package br.com.sandes.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.com.sandes.data.vo.v1.BooksVO;
-import br.com.sandes.data.vo.v1.PersonVO;
 import br.com.sandes.services.BookService;
 import br.com.sandes.util.MediaType;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,11 +22,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RequestMapping("/api/books/v1/")
 @RestController
