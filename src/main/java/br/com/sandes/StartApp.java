853acfb0552595f7d948677871e1fcd04b6388a5
Mateus Sandes <mateussandes@hotmail.com.br>
2024-04-05 19:01:03 -0300

    Exibindo o hash das senhas encriptadas no console da aplicação

--- src/main/java/br/com/sandes/StartApp.java
+++ src/main/java/br/com/sandes/StartApp.java
@@ -1,7 +1,13 @@
 package br.com.sandes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class StartApp {
@@ -9,25 +15,18 @@ public class StartApp {
 	public static void main(String[] args) {
 		SpringApplication.run(StartApp.class, args);
 		
-//		Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = 
-//				new Pbkdf2PasswordEncoder("",
-//						8,
-//						185000,
-//						SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-//		
-//        Map<String, PasswordEncoder> encoders = new HashMap<>();
-//        
-//        encoders.put("pbkdf2", pbkdf2PasswordEncoder);
-//        
-//        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-//        
-//        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2PasswordEncoder);
-//        
-//        String result1 = passwordEncoder.encode("admin123");
-//        String result2 = passwordEncoder.encode("admin234");
-//        
-//        System.out.println("My hash " + result1);
-//        System.out.println("My hash " + result2);
+		Map<String, PasswordEncoder> encoders = new HashMap<>();
+		 
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		 
+		encoders.put("BCryptPasswordEncoder", encoder);
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("BCryptPasswordEncoder", encoders);
+		passwordEncoder.setDefaultPasswordEncoderForMatches(encoder);
+		 
+		String result1 = passwordEncoder.encode("teste");
+		String result2 = passwordEncoder.encode("teste");
+		System.out.println("My hash result1 " + result1);
+		System.out.println("My hash result2 " + result2); 
 	}
 
 }
