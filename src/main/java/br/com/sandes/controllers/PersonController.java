c854207fd8801560976ef30aeddb9ec61be1ca29
Mateus Sandes <mateussandes@hotmail.com.br>
2024-04-05 18:59:12 -0300

    Corrigido o path do controller

--- src/main/java/br/com/sandes/controllers/PersonController.java
+++ src/main/java/br/com/sandes/controllers/PersonController.java
@@ -25,7 +25,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/api/person/v1/")
+@RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for Managing People")
 public class PersonController {
 
