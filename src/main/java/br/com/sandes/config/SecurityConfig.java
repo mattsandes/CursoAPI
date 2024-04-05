61859608902d989c7d0affe5e822b59fad179339
Mateus Sandes <mateussandes@hotmail.com.br>
2024-04-05 19:00:34 -0300

    Trocada a classe atual de encriptação pela classe BCrypt de encriptação

--- src/main/java/br/com/sandes/config/SecurityConfig.java
+++ src/main/java/br/com/sandes/config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package br.com.sandes.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -29,26 +24,26 @@ public class SecurityConfig {
 	@Autowired
 	private JwtTokenProvider tokenProvider;
 	
-	//esse meotod vai dizer ao spring como encriptar a senha para comparar com a senha que esta no banco;
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		Map<String, PasswordEncoder> encoders = new HashMap<String, PasswordEncoder>();
-		
-		Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = 
-				new Pbkdf2PasswordEncoder("",
-						8,
-						185000,
-						SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-		
-		encoders.put("pbkdf2", pbkdf2PasswordEncoder);
-		
-		DelegatingPasswordEncoder passwordEncoder = 
-				new DelegatingPasswordEncoder("pbkdf2", encoders);
-		
-		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2PasswordEncoder);
-		
-		return passwordEncoder;
-	}
+	//esse meotodo vai dizer ao spring como encriptar a senha para comparar com a senha que esta no banco;
+//	@Bean
+//	PasswordEncoder passwordEncoder() {
+//		Map<String, PasswordEncoder> encoders = new HashMap<String, PasswordEncoder>();
+//		
+//		Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = 
+//				new Pbkdf2PasswordEncoder("",
+//						8,
+//						185000,
+//						SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+//		
+//		encoders.put("pbkdf2", pbkdf2PasswordEncoder);
+//		
+//		DelegatingPasswordEncoder passwordEncoder = 
+//				new DelegatingPasswordEncoder("pbkdf2", encoders);
+//		
+//		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2PasswordEncoder);
+//		
+//		return passwordEncoder;
+//	}
 	
 	@Bean
 	AuthenticationManager authenticationManagerBean(
@@ -71,15 +66,19 @@ public class SecurityConfig {
 			.authorizeHttpRequests(
 					authorizeHttpRequests -> authorizeHttpRequests
 						.requestMatchers(
-								"/auth/signin",
-								"/auth/refresh/**",
+								"/auth/**",
 								"/swagger-ui/**",
 								"/v3/api-docs/**"
 						).permitAll()
 						.requestMatchers("/api/**").authenticated()
 						.requestMatchers("/users").denyAll()
 			)
-			.cors(cors -> {})
+			.cors(cors -> cors.disable())
 			.build();
-	}	
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
