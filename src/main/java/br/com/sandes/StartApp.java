c1d7827684d7e32b972ce0c0140ca4dc7359f6dd
Mateus Sandes <mateussandes@hotmail.com.br>
2024-04-06 10:46:28 -0300

    Retornando o hash da senha no console da aplicação

--- src/main/java/br/com/sandes/StartApp.java
+++ src/main/java/br/com/sandes/StartApp.java
@@ -5,9 +5,10 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 
 @SpringBootApplication
 public class StartApp {
@@ -16,17 +17,18 @@ public class StartApp {
 		SpringApplication.run(StartApp.class, args);
 		
 		Map<String, PasswordEncoder> encoders = new HashMap<>();
-		 
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		 
-		encoders.put("BCryptPasswordEncoder", encoder);
-		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("BCryptPasswordEncoder", encoders);
-		passwordEncoder.setDefaultPasswordEncoderForMatches(encoder);
-		 
-		String result1 = passwordEncoder.encode("teste");
-		String result2 = passwordEncoder.encode("teste");
+
+		Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000,
+				SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+
+		encoders.put("pbkdf2", pbkdf2Encoder);
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+
+		String result1 = passwordEncoder.encode("admin123");
+		String result2 = passwordEncoder.encode("admin234");
 		System.out.println("My hash result1 " + result1);
-		System.out.println("My hash result2 " + result2); 
+		System.out.println("My hash result2 " + result2);
 	}
 
 }
