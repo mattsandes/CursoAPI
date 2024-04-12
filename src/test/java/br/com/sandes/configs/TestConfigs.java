0b3432f693f0e87129a54295543376930d97111d
Mateus Sandes <mateussandes@hotmail.com.br>
2024-04-12 15:52:21 -0300

    Adcionado outras configurações para os testes

--- src/test/java/br/com/sandes/configs/TestConfigs.java
+++ src/test/java/br/com/sandes/configs/TestConfigs.java
@@ -2,5 +2,21 @@ package br.com.sandes.configs;
 
 public class TestConfigs {
 
+	//definindo a porta que o restAssured usara quando os testes de integração for iniciado;
 	public static final int SERVER_PORT = 8888;
+	
+	//adicionando a constante de origem;
+	public static final String HEADER_PARAM_ORIGIN = "Origin";
+	
+	//contante para autenticação;
+	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+	
+	//constante para tipo de conteudo;
+	public static final String CONTENT_TYPE_JSON = "application/json";
+	public static final String CONTENT_TYPE_XML = "application/xml";
+	public static final String CONTENT_TYPE_YAML = "application/x-yaml";
+	
+	//variaeis de origem
+	public static final String ORIGIN_ERUDIO = "https://erudio.com.br";
+	public static final String ORIGIN_SEMERU = "https://semeru.com.br";
 }
