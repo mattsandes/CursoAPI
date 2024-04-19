6904184013b3b9c247da8a4653eab5fb6e2751bc
Mateus Sandes <mateussandes@hotmail.com.br>
2024-04-19 11:46:11 -0300

    Criada a classe de teste para o controller de Person

--- src/test/java/br/com/sandes/integrationtests/controllers/witJson/PersonControllerJsonTest.java
+++ src/test/java/br/com/sandes/integrationtests/controllers/witJson/PersonControllerJsonTest.java
@@ -30,45 +30,45 @@ import io.restassured.specification.RequestSpecification;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestMethodOrder(OrderAnnotation.class)
-public class PersonControllerJsonTest extends AbstractIntegrationTest{
+public class PersonControllerJsonTest extends AbstractIntegrationTest {
 
 	private static RequestSpecification specification;
 	private static ObjectMapper mapper;
-	
+
 	private static PersonVO person;
-	
+
 	@BeforeAll
 	static void setUp() {
 		mapper = new ObjectMapper();
-		
-		//essa propriedade vai desabilitar falhas quando o
-		//teste encontrar propriedades que nao estao pesentes no escopo do vo;
+
+		// essa propriedade vai desabilitar falhas quando o
+		// teste encontrar propriedades que nao estao pesentes no escopo do vo;
 		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-		
+
 		person = new PersonVO();
 	}
-	
+
 	@Test
 	@Order(0)
 	public void authorization() throws JsonMappingException, JsonProcessingException {
-		//instancia de account credentials que sera a request feita para o endpint;
+		// instancia de account credentials que sera a request feita para o endpint;
 		AccountCredentialsVO user = new AccountCredentialsVO("leandro", "admin123");
-		
-		//implementação do teste
+
+		// implementação a requisição;
 		var accessToken = given()
-				.basePath("/auth/signin") //path para fazer a requisição;
-					.port(TestConfigs.SERVER_PORT) //porta que o teste ser executada;
-					.contentType(TestConfigs.CONTENT_TYPE_JSON)
+				.basePath("/auth/signin") // path para fazer a requisição;
+				.port(TestConfigs.SERVER_PORT) // porta que o teste ser executada;
+				.contentType(TestConfigs.CONTENT_TYPE_JSON)
 				.body(user)
 					.when()
-				.post()
+						.post()
 					.then()
 						.statusCode(200)
-							.extract()
-							.body()
-								.as(TokenVO.class)
-							.getAccessToken();
-		
+					.extract()
+						.body()
+							.as(TokenVO.class) // extraindo o corpo da requisição como um objeto
+				.getAccessToken();
+
 		specification = new RequestSpecBuilder()
 				.addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + accessToken)
 				.setBasePath("/api/person/v1")
@@ -82,38 +82,38 @@ public class PersonControllerJsonTest extends AbstractIntegrationTest{
 	@Order(1)
 	public void testCreate() throws JsonMappingException, JsonProcessingException {
 		mockPerson();
-		
-		//implementação do teste
+
+		// implementação do teste
 		var content = given().spec(specification)
+				.header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_ERUDIO)
 				.contentType(TestConfigs.CONTENT_TYPE_JSON)
-					.header(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_ERUDIO)
-					.body(person)
-					.when()
-					.post()
-				.then()
-					.statusCode(200)
-				.extract()
-					.body()
-						.asString();
-		
-		//lendo o objeto criado;
+				.body(person)
+					.when().
+						post()
+					.then()
+						.statusCode(200)
+					.extract()
+						.body()
+							.asString();
+
+		// lendo o objeto criado;
 		PersonVO createdPerson = mapper.readValue(content, PersonVO.class);
 		person = createdPerson;
-		
+
 		assertTrue(createdPerson.getId() > 0);
 		assertNotNull(createdPerson.getId());
 		assertNotNull(createdPerson.getFirst_name());
 		assertNotNull(createdPerson.getLast_name());
 		assertNotNull(createdPerson.getGender());
 		assertNotNull(createdPerson.getAddress());
-		
+
 		assertEquals("Richard", createdPerson.getFirst_name());
 		assertEquals("Stallman", createdPerson.getLast_name());
 		assertEquals("Male", createdPerson.getGender());
 		assertEquals("New York City, New York, US", createdPerson.getAddress());
 	}
 
-	//criando um metodo que vai emular o person;
+	// criando um metodo que vai emular o person;
 	private void mockPerson() {
 		person.setFirst_name("Richard");
 		person.setLast_name("Stallman");
