ab60cfdd32b82ecb83da32c31da3753cb4b332cb
Mateus Sandes <mateussandes@hotmail.com.br>
2024-04-25 02:55:56 -0300

    Criandos os endpoints para upload e download de arquivos

--- src/main/java/br/com/sandes/controllers/PersonController.java
+++ src/main/java/br/com/sandes/controllers/PersonController.java
@@ -76,6 +76,46 @@ public class PersonController {
     	return ResponseEntity.ok(personServices.findAll(pageable));
     }
 
+    @GetMapping(value = "/findPersonByName/{firstName}",
+            produces = {MediaType.APPLICATION_JSON,
+            MediaType.APPLICATION_XML,
+            "application/x-yaml"})
+    @Operation(summary = "Finds people by name", description = "Finds people by name",
+            tags = {"People"},
+            responses = {
+                @ApiResponse(description = "Success", responseCode = "200",
+                        content = {
+                                @Content(
+                                        mediaType = "application/json",
+                                        array = @ArraySchema(schema = @Schema(implementation = PersonVO.class)))
+                        }),
+                @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            }
+    )
+    public ResponseEntity<PagedModel<EntityModel<PersonVO>>> findPersonsByName(
+        @RequestParam(value = "page", defaultValue = "0") Integer page,
+        @RequestParam(value = "limit", defaultValue = "12") Integer limit,
+        @RequestParam(value = "direction", defaultValue = "asc") String direction,
+        @PathVariable(value = "firstName") String firstName){
+
+                //definindo o sortDirection
+                //isso servira para indicar como os resultados da pagina serão ordenados
+
+                //esse trecho valida se na requisição, o parametro sort direction é igual a
+                //desc, se for 'desc' os resultados serão exibidos de maneira descnedente
+                //caso seja diferente disso, os valores serão exibidos de maneira ascendente;
+        var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+
+        //defiinindo a numero de paginas, a quantidade de itens na pagina e a ordenação
+        //se ascendente ou descendente;
+        Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "firstName"));
+
+    	return ResponseEntity.ok(personServices.findPersonByName(firstName, pageable));
+    }
+
     @CrossOrigin(origins = "http://localhost:8080") //como isso, apenas os acessos desse endereço poderao acessar esse endoint;
     @GetMapping(value = "/{id}",
             produces = {MediaType.APPLICATION_JSON,
